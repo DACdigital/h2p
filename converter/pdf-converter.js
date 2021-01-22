bc5258449c8f1fad5e0f2df0114bebaf9ddd7714
pwojciechowski <patryk.wojciechowski@dac.digital>
2021-01-22 10:05:30 +0100

    [muumap-support-247] readme update

--- converter/pdf-converter.js
+++ converter/pdf-converter.js
@@ -1,10 +1,6 @@
 const saveToPdf = async (page, html, options) => {
     await page.setContent(html);
 
-    /*
-     For the options to pdf function please look at the docs
-     https://devdocs.io/puppeteer/index#pagepdfoptions
-    */
     printOpts = {
         format: 'A4',
         ...options
